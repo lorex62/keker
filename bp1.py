480998f005ca879c8e61d9e2a2e3e0661654d05d
lorex62 <58516445+lorex62@users.noreply.github.com>
2020-02-19 16:12:48 +0300

    Add files via upload

---
+++ bp1.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+import pygame
+import requests
+
+coords = input('Введите координаты через запятую без пробелов:').strip()
+zoomout = input('Введите маштаб(от 0 до 17):')
+response = None
+map_request = "https://static-maps.yandex.ru/1.x/?ll={0}&size=450,450&z={1}&l=sat".format(coords, zoomout)
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((450, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
